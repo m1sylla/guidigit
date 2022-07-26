a2b8a423c4fa50037101c495cc7f7925534b528f
Mohamed SYLLA <mohamed111sylla@gmail.com>
2022-07-26 16:12:22 +0000

    commit of 26 07 2022

--- js/custom.js
+++ js/custom.js
@@ -1,52 +1,61 @@
-
-  $(function () {
+$(function() {
 
     // MENU
-    $('.nav-link').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
+    $('.nav-link').on('click', function() {
+        $(".navbar-collapse").collapse('hide');
     });
 
 
     // AOS ANIMATION
     AOS.init({
-      disable: 'mobile',
-      duration: 800,
-      anchorPlacement: 'center-bottom'
+        disable: 'mobile',
+        duration: 800,
+        anchorPlacement: 'center-bottom'
     });
 
 
     // SMOOTH SCROLL
     $(function() {
-      $('.nav-link').on('click', function(event) {
-        var $anchor = $(this);
-          $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 0
-          }, 1000);
+        $('.nav-link').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 0
+            }, 1000);
             event.preventDefault();
-      });
-    });  
+        });
+    });
 
 
     // PROJECT SLIDE
     $('#project-slide').owlCarousel({
-      loop: true,
-      center: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      margin: 30,
-      responsiveClass:true,
-      responsive:{
-          0:{
-              items:1,
-          },
-          768:{
-              items:2,
-          }
-      }
+        loop: true,
+        center: true,
+        autoplayHoverPause: false,
+        autoplay: true,
+        margin: 30,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            }
+        }
     });
 
-  });
+});
 
+/*************************
+ * BLOG
+ ***************************/
 
-    
-
+// SOCIAL SHARE
+let shareButton = document.querySelectorAll(".share-popup");
+let windowsSize = "";
+for (let i = 0; i < shareButton.length; i++) {
+    shareButton[i].addEventListener("click", function() {
+        window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+        return false;
+    });
+}
